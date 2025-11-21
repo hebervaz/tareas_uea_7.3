69d6f3cb1a1ec5b401c100dee82b9f90273c2a80
Heber Vazquez <gerber@Laptop-de-Heber.local>
2025-11-20 23:25:52 -0600

    Actualización de repositorio

--- clas7/supabase.js
+++ clas7/supabase.js
@@ -1,5 +1,8 @@
 
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
+
 
 // Crea un unico cliente supabase para interactuar con tu base de datos
-const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+const supa = supabase.createClient('https://otefeicxbgkpgetolfvv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90ZWZlaWN4YmdrcGdldG9sZnZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTU2MjYsImV4cCI6MjA3NzQ5MTYyNn0.LiOw9h0tn6gDs4EOqIhAWdq386qVzHpy4AHgwlFhArM')
+
+console.log(supa)
